d5b9d3ba21f8701226412af5e5b6eba43dc3e814
dang9805 <congdang981997@gmail.com>
2025-11-06 20:22:02 +0700

    update role ke toan v2

--- FE/src/pages/Accountant/AccountPayment.jsx
+++ FE/src/pages/Accountant/AccountPayment.jsx
@@ -76,7 +76,7 @@ const InvoiceFormModal = ({ isOpen, onClose, onSave, invoiceData, error, setErro
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+    <div className="fixed inset-0 bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white p-6 rounded-lg w-full max-w-md text-gray-900">
         <h2 className="text-lg font-bold mb-4">
           {isEditing ? "Chỉnh sửa hóa đơn" : "Thêm hóa đơn mới"}
