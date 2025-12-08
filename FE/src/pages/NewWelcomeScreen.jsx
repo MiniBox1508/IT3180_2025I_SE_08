2e6965f94b01d2b2573fe5e971e7c8e9d241931d
dang9805 <congdang981997@gmail.com>
2025-12-08 20:56:13 +0700

    update quen mk v2

--- FE/src/pages/NewWelcomeScreen.jsx
+++ FE/src/pages/NewWelcomeScreen.jsx
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import BackgroundImage from "../images/background_welcome.jpg";
-
-// --- API CONFIG ---
-const API_BASE_URL = "https://testingdeploymentbe-2.vercel.app";
-
-// --- ICONS ---
-const UserIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-blue-300" viewBox="0 0 20 20" fill="currentColor">
-    <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
-  </svg>
-);
-
-const LockClosedIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-blue-300" viewBox="0 0 20 20" fill="currentColor">
-    <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-  </svg>
-);
+import BackgroundImage from "../images/new_welcome_background.jpg";
 
 // --- COMPONENT CHÍNH ---
 export const NewWelcomeScreen = () => {
+    // --- API CONFIG ---
+  const API_BASE_URL = "https://testingdeploymentbe-2.vercel.app";
+
+  // --- ICONS ---
+  const UserIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-blue-300" viewBox="0 0 20 20" fill="currentColor">
+      <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+    </svg>
+  );
+
+  const LockClosedIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-blue-300" viewBox="0 0 20 20" fill="currentColor">
+      <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+    </svg>
+  );
+
   const [apartmentId, setApartmentId] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
