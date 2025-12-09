6234afe1ea815627f01646fc1a5ced098503b15c
dang9805 <congdang981997@gmail.com>
2025-12-09 17:04:59 +0700

    add BQT report v3

--- FE/src/layouts/SharedLayout.jsx
+++ FE/src/layouts/SharedLayout.jsx
@@ -16,7 +16,7 @@ const navItems = [
   { name: "Trang chủ", to: "/management", icon: iconTrangChu },
   { name: "Dân cư", to: "/management/residents", icon: iconDanCu },
   { name: "Dịch vụ", to: "/management/services", icon: iconDichVu },
-  { name: "Báo cáo", to: "/management/report", icon: iconThanhToan },
+  { name: "Báo cáo", to: "/management/reports", icon: iconThanhToan },
   { name: "Thanh toán", to: "/management/payments", icon: iconThanhToan },
   { name: "Thông báo", to: "/management/notifications", icon: iconThongBao },
 ];
