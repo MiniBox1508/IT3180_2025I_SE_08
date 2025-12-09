c557b1704430fc6969b522d2ef0be192cd79f093
MiniBox1508 <hungbeo7a@gmail.com>
2025-12-09 22:26:38 +0700

    Update Services Page BQT 6

--- FE/src/pages/BQT/ServicesPage.jsx
+++ FE/src/pages/BQT/ServicesPage.jsx
@@ -68,13 +68,10 @@ const ServicesPage = () => {
           </div>
           {/* Title & Actions */}
           <div className="flex justify-between items-center">
-            <h1 className="text-3xl font-bold text-slate-800">Dịch vụ</h1>
+            <h1 className="text-3xl font-bold text-white">Dịch vụ</h1>
             <div className="flex gap-3">
-              <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded font-bold shadow">
-                + Đăng ký dịch vụ
-              </button>
               <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded font-bold shadow">
-                Phản ánh dịch vụ
+                Phản hồi
               </button>
               <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded font-bold shadow">
                 Xóa dịch vụ
