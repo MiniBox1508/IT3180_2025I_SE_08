a2702e80e3a205bd0702f7d9e72b46cb7be8081c
MiniBox1508 <hungbeo7a@gmail.com>
2025-12-30 01:08:00 +0700

    test

--- FE/src/pages/Accountant/AccountNotification.jsx
+++ FE/src/pages/Accountant/AccountNotification.jsx
@@ -776,8 +776,8 @@ export const AccountantNotification = () => {
         if (totalFail === 0 && successCount > 0)
           message = `Nhập thành công ${successCount} thông báo!`;
         else if (successCount > 0)
-          message = `Nhập thành công ${successCount} thông báo!\n
-                    Nhập thất bại ${totalFail} thông báo!`;
+          message = `Nhập thành công ${successCount} thông báo!
+          Nhập thất bại ${totalFail} thông báo!`;
         else {
           message = "Nhập thất bại toàn bộ.";
           type = "failure";
